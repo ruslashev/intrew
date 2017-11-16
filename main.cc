7663adc84d3028a6b30fe425dd76ac7758e013d6
Ruslan Akbashev <ruslashev@gmail.com>
2017-11-17 00:13:04 +0300

    Add merge sort

--- main.cc
+++ main.cc
@@ -1,11 +1,11 @@
-#include "insertion_sort.hh"
+#include "merge_sort.hh"
 #include "utils.hh"
 
 int main() {
   seed();
   vi a = { 5, 2, 4, 6, 1, 3 }; // generate_array_random_unique<int>(20);
   print_vector(a);
-  insertion_sort(&a);
+  merge_sort(&a);
   print_vector(a);
 }
 
---
+++ merge_sort.hh
@@ -0,0 +1,68 @@
+#pragma once
+
+#include "simple.hh"
+
+// warning: uses 'inf' even though vector elements are templated
+template <typename T>
+static void merge_unused(std::vector<T> *A, int p, int q, int r) {
+  int n1 = q - p + 1, n2 = r - q;
+  std::vector<T> L(n1 + 1), R(n2 + 1);
+  for (int i = 1; i <= n1; ++i)
+    L[i - 1] = (*A)[p + i - 2];
+  for (int j = 1; j <= n2; ++j)
+    R[j - 1] = (*A)[q + j - 1];
+  L[n1] = inf;
+  R[n2] = inf;
+  int i = 1, j = 1;
+  for (int k = p; k <= r; ++k)
+    if (L[i - 1] <= R[j - 1]) {
+      (*A)[k - 1] = L[i - 1];
+      ++i;
+    } else {
+      (*A)[k - 1] = R[j - 1];
+      ++j;
+    }
+}
+
+template <typename T>
+static void merge(std::vector<T> *A, int p, int q, int r) {
+  int n1 = q - p + 1, n2 = r - q;
+  std::vector<T> L(n1 + 1), R(n2 + 1);
+  for (int i = 1; i <= n1; ++i)
+    L[i - 1] = (*A)[p + i - 2];
+  for (int j = 1; j <= n2; ++j)
+    R[j - 1] = (*A)[q + j - 1];
+  L[n1] = inf;
+  R[n2] = inf;
+  int i = 1, j = 1, k = p;
+  while (i <= n1 && j <= n2) {
+    if (L[i - 1] <= R[j - 1]) {
+      (*A)[k - 1] = L[i - 1];
+      ++i;
+    } else {
+      (*A)[k - 1] = R[j - 1];
+      ++j;
+    }
+    ++k;
+  }
+  while (i != n1 + 1)
+    (*A)[k++ - 1] = L[i++ - 1];
+  while (j != n2 + 1)
+    (*A)[k++ - 1] = R[j++ - 1];
+}
+
+template <typename T>
+static void merge_sort(std::vector<T> *A, int p, int r) {
+  if (p < r) {
+    int q = (p + r) / 2;
+    merge_sort(A, p, q);
+    merge_sort(A, q + 1, r);
+    merge(A, p, q, r);
+  }
+}
+
+template <typename T>
+void merge_sort(std::vector<T> *A) {
+  merge_sort(A, 1, A->size());
+}
+
