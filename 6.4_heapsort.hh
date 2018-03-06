a2a7e07a1de0e064ab74b07014a8fac6cba5d9de
Ruslan Akbashev <ruslashev@gmail.com>
2018-03-06 23:22:26 +0300

    fixx heapsort

--- 6.4_heapsort.hh
+++ 6.4_heapsort.hh
@@ -1,44 +1,55 @@
 #pragma once
 #include "util.hh"
 
-size_t _left(size_t i) {
+size_t left(size_t i) {
   return 2 * i;
 }
 
-size_t _right(size_t i) {
+size_t right(size_t i) {
   return 2 * i + 1;
+}
+
+template <typename T>
+void max_heapify_rec(vector<T> &a, size_t heap_size, size_t i) {
+  size_t l = left(i), r = right(i)
+    , largest = (l < heap_size && a[l] > a[i]) ? l : i;
+  if (r < heap_size && a[r] > a[largest])
+    largest = r;
+  if (largest != i) {
+    std::swap(a[i], a[largest]);
+    max_heapify_rec(a, heap_size, largest);
+  }
 }
 
 template <typename T>
 void max_heapify(vector<T> &a, size_t heap_size, size_t i) {
   while (i < heap_size) {
-    size_t l = _left(i), r = _right(i)
-      , largest = (l <= heap_size && a[l - 1] > a[i - 1]) ? l : i;
-    if (r <= heap_size && a[r - 1] > a[largest - 1])
+    size_t l = left(i), r = right(i)
+      , largest = (l < heap_size && a[l] > a[i]) ? l : i;
+    if (r < heap_size && a[r] > a[largest])
       largest = r;
-    if (largest != i) {
-      std::swap(a[i - 1], a[largest - 1]);
-      i = largest;
-    } else
+    if (largest == i)
       break;
+    std::swap(a[i], a[largest]);
+    i = largest;
   }
 }
 
 template <typename T>
 size_t build_max_heap(vector<T> &a) {
   size_t heap_size = a.size();
-  for (size_t i = heap_size / 2; i >= 1; --i)
+  for (int i = heap_size / 2; i >= 0; --i)
     max_heapify(a, heap_size, i);
   return heap_size;
 }
 
 template <typename T>
 void heapsort(vector<T> &a) {
-  size_t heap_size = build_max_heap(a, heap_size);
-  for (size_t i = a.size(); i >= 2; --i) {
-    std::swap(a[0], a[i - 1]);
+  size_t heap_size = build_max_heap(a);
+  for (size_t i = a.size() - 1; i >= 1; --i) {
+    std::swap(a[0], a[i]);
     --heap_size;
-    max_heapify(a, heap_size, 1);
+    max_heapify(a, heap_size, 0);
   }
 }
 
--- main.cc
+++ main.cc
@@ -1,21 +1,9 @@
-#include "4.2_matrix_mult.hh"
+#include "6.4_heapsort.hh"
 
 int main() {
-  vector<vector<int>> a = {
-    {  6, -4,  4, 2 },
-    {  7,  3,  8, 2 },
-    {  5,  1,  9, 2 },
-    {  2,  1,  9, 2 }
-  }, b = {
-    {  2,  6,  4, 3 },
-    { -2,  7,  3, 3 },
-    { -3,  1, -4, 3 },
-    {  2,  3,  9, 2 }
-  }, c = mat_mult_strassen(a, b);
-  for (size_t y = 0; y < c.size(); ++y) {
-    for (size_t x = 0; x < c[y].size(); ++x)
-      printf("%d ", c[y][x]);
-    puts("");
-  }
+  vector<int> a = generate_vector_random<int>(15, 0, 15);
+  print_vector(a);
+  heapsort(a);
+  print_vector(a);
 }
 
