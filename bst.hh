2af95b6f9c4f565814ad21f94a069cce275204ea
Ruslan Akbashev <ruslashev@gmail.com>
2018-04-19 18:37:14 +0300

    Fix bst and print it

--- bst.hh
+++ bst.hh
@@ -1,13 +1,15 @@
 #pragma once
 
 #include <iostream>
+#include <vector>
+#include <string>
 
 template <typename T>
 struct bst_node {
-  T data;
+  T key;
   bst_node *left, *right, *parent;
 
-  bst_node(const T &_data, bst_node *_parent = nullptr) : data(_data),
+  bst_node(const T &_key, bst_node *_parent = nullptr) : key(_key),
       left(nullptr), right(nullptr), parent(_parent) {}
 };
 
@@ -16,28 +18,63 @@ struct bst {
   bst_node<T> *root;
 
   bst() : root(nullptr) {}
-  void insert(const T &data) {
-    bst_node<T> *parent, *ins_ptr = root;
+
+  void insert(const T &key) {
+    bst_node<T> *parent = nullptr, *ins_ptr = root;
     while (ins_ptr != nullptr) {
       parent = ins_ptr;
-      ins_ptr = (data < ins_ptr->data) ? ins_ptr->left : ins_ptr->right;
+      ins_ptr = (key < ins_ptr->key) ? ins_ptr->left : ins_ptr->right;
     }
-    bst_node<T> *new_node = new bst_node<T>(data, parent);
+    bst_node<T> *new_node = new bst_node<T>(key, parent);
     if (parent == nullptr)
       root = new_node;
     else {
-      if (data < parent->data)
+      if (key < parent->key)
         parent->left = new_node;
       else
         parent->right = new_node;
     }
   }
+
   void pre_order(bst_node<T> *x) {
     if (!x)
       return;
     pre_order(x->left);
     pre_order(x->right);
-    std::cout << x->data << std::endl;
+    std::cout << x->key << std::endl;
+  }
+
+  void print(bst_node<T> *x, int type, const std::string &indent) {
+    if (!x)
+      return;
+
+    std::string right_indent = indent, left_indent = indent;
+    if (type == 1) {
+      right_indent += "     ";
+      left_indent += " |   ";
+    }
+    if (type == -1) {
+      left_indent += "     ";
+      right_indent += " |   ";
+    }
+
+    print(x->right, 1, right_indent);
+
+    std::cout << indent;
+    if (type == 1)
+      std::cout << " /---";
+    if (type == -1)
+      std::cout << " \\---";
+    std::cout << x->key << std::endl;
+
+    print(x->left, -1, left_indent);
+  }
+
+  void print() {
+    if (root == nullptr)
+      std::cout << "<null>" << std::endl;
+    else
+      print(root, 0, "");
   }
 };
 
--- main.cc
+++ main.cc
@@ -2,13 +2,24 @@
 
 int main() {
   bst<int> b;
-  b.insert(10);
+
   b.insert(8);
-  b.insert(12);
   b.insert(4);
-  b.insert(9);
-  b.insert(13);
+  b.insert(12);
+  b.insert(2);
+  b.insert(6);
+  b.insert(10);
   b.insert(14);
-  b.pre_order(b.root);
+  b.insert(1);
+  b.insert(3);
+  b.insert(5);
+  b.insert(7);
+  b.insert(9);
+  b.insert(11);
+  b.insert(13);
+  b.insert(20);
+  b.insert(15);
+
+  b.print();
 }
 
