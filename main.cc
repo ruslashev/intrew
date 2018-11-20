8eff659c6260ca32fc3cb795b1abc3046a6bd7f9
Ruslan Akbashev <ruslashev@gmail.com>
2018-11-20 22:41:02 +0300

    add rod cutting from old/

--- main.cc
+++ main.cc
@@ -1,7 +1,12 @@
 #include "all.hh"
 
 int main() {
-  v a = generate_vector_random_walk<int>(10, 5, 10);
-  print_vector(a);
+  int n = 18;
+  v p = generate_vector_random_walk<int>(n, 1, 10);
+  print_vector(p);
+  time_execution(rod_cutting1(p, n));
+  time_execution(rod_cutting2(p, n));
+  time_execution(rod_cutting3(p, n));
+  time_execution(rod_cutting4(p, n));
 }
 
