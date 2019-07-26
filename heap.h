d410cf03e43c6f4b74bdf8ddd8d304859459a09b
Ruslan Akbashev <r.akbashev@partner.samsung.com>
2019-07-26 17:44:45 +0300

    add max-subarray, heapsort & prio queue

---
+++ heap.h
@@ -0,0 +1,121 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define die(...) do { printf(__VA_ARGS__); puts(""); exit(1); } while (0)
+
+int parent(int i)
+{
+    return i / 2;
+}
+
+int left_child(int i)
+{
+    return 2 * i;
+}
+
+int right_child(int i)
+{
+    return 2 * i + 1;
+}
+
+void swap(int *x, int *y)
+{
+    int t = *x;
+    *x = *y;
+    *y = t;
+}
+
+void maintain_heap_prop_for_element(int *a, int h, int i)
+{
+    int lc, rc, largest;
+
+    while (i < h) {
+        lc = left_child(i);
+        rc = right_child(i);
+
+        largest = (lc < h && a[lc] > a[i]) ? lc : i;
+
+        if (rc < h && a[rc] > a[largest])
+            largest = rc;
+
+        if (largest == i)
+            break;
+
+        swap(&a[i], &a[largest]);
+
+        i = largest;
+    }
+}
+
+void build_max_heap(int *a, int n)
+{
+    for (int i = n / 2; i >= 0; --i)
+        maintain_heap_prop_for_element(a, n, i);
+}
+
+int heap_maximum(int *a)
+{
+    return a[0];
+}
+
+int heap_extract_max(int *a, int n, int *h)
+{
+    if (*h < 1)
+        die("heap underflow");
+
+    int max = a[0];
+
+    a[0] = a[*h - 1];
+    *h = *h - 1;
+    maintain_heap_prop_for_element(a, n, 0);
+
+    return max;
+}
+
+void heap_increase_key(int *a, int i, int key)
+{
+    if (key < a[i])
+        return;
+
+    a[i] = key;
+
+    while (i > 0 && a[parent(i)] < a[i]) {
+        swap(&a[i], &a[parent(i)]);
+
+        i = parent(i);
+    }
+}
+
+void max_heap_insert(int *a, int *h, int key)
+{
+    int i;
+
+    *h = *h + 1;
+
+    i = *h - 1;
+
+    a[i] = key;
+
+    while (i > 0 && a[parent(i)] < a[i]) {
+        swap(&a[i], &a[parent(i)]);
+
+        i = parent(i);
+    }
+}
+
+void heapsort(int *a, int n)
+{
+    int heap_size = n;
+
+    build_max_heap(a, n);
+
+    for (int i = n - 1; i >= 1; --i) {
+        swap(&a[0], &a[i]);
+
+        --heap_size;
+        maintain_heap_prop_for_element(a, heap_size, 0);
+    }
+}
+
---
+++ max-subarray.h
@@ -0,0 +1,70 @@
+#pragma once
+
+#include <limits.h>
+
+const int neginf = INT_MIN;
+
+struct subarray
+{
+    int start, end, sum;
+};
+
+int max(int x, int y)
+{
+    return (x > y) ? x : y;
+}
+
+int max_subarray_crossing_midpoint(int *a, int l, int m, int h)
+{
+    int i, s, left_sum = neginf, right_sum = neginf;
+
+    for (i = m, s = 0; i >= l; --i) {
+        s += a[i - 1];
+        left_sum = max(left_sum, s);
+    }
+
+    for (i = m + 1, s = 0; i <= h; ++i) {
+        s += a[i - 1];
+        right_sum = max(right_sum, s);
+    }
+
+    return left_sum + right_sum;
+}
+
+int max_subarray(int *a, int l, int h)
+{
+    int m, left_sum, cross_sum, right_sum;
+
+    if (l == h)
+        return a[l - 1];
+
+    m = (l + h) / 2;
+
+    left_sum = max_subarray(a, l, m);
+    right_sum = max_subarray(a, m + 1, h);
+    cross_sum = max_subarray_crossing_midpoint(a, l, m, h);
+
+    return max(left_sum, max(cross_sum, right_sum));
+}
+
+struct subarray max_subarray_linear(int *a, int len)
+{
+    int beg = 0, end = 0;
+    int w = a[0], max_sum = a[0];
+
+    for (int i = 1; i < len; ++i) {
+        if (a[i] > w + a[i]) {
+            beg = end = i;
+            w = a[i];
+        } else
+            w += a[i];
+
+        if (w > max_sum) {
+            max_sum = w;
+            end = i;
+        }
+    }
+
+    return { beg, end, max_sum };
+}
+
